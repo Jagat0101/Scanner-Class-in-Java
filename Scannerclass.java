7dd373dc1d616a169db00be29df6a4ea648d3a1c
JAGAT JEEVAN MAHAPATRA <jagatjeevanmahapatra055@gmail.com>
2025-07-01 15:32:07 +0530

    Update and rename Scanner.java to Scannerclass.java

--- Scannerclass.java
+++ Scannerclass.java
@@ -1,18 +1,20 @@
 
 import java.util.Scanner;
 
-public class Main1 {
+public class Scannerclass {
     public static void main(String[] args) {
         System.out.println("This is the first lesson of java");
         System.out.println("Here will learn Scanner class");
+        System.out.println("Enter a number");
+        System.out.println("Enter your name");
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        int a = input.nextInt(); // nextInt() takes integer values of user
         System.out.println(a);
         
         System.out.println();
-        System.out.println();
+        System.out.println();// Added some extra line to make the output more presentable 
         
-        String b = input.nextLine();
+        String b = input.nextLine();//nextLine takes String values as input 
         System.out.println(b);
     }
     
